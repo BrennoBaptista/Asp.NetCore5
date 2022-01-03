dc1f3f621817d108a0075c7294e314f5bd1e24f5
Brenno Baptista <brenno_baptista@hotmail.com>
2022-01-03 18:23:59 -0300

    Person service implemented with mocks

---
+++ RestApi/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
--- RestApi/Startup.cs
+++ RestApi/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestApi.Services;
+using RestApi.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,8 +27,8 @@ namespace RestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
